a624f0f1732e4b86e13147ec7e7ba8d5c9fb9d80
Jonat <Jonathon.A.Stamper@gmail.com>
2022-03-24 13:42:21 +0100

    test2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class App {
 
 	public static void main(String[] args){
@@ -6,6 +8,25 @@ public class App {
 }
 
 class Student{
+private String Naam;
+private static Integer studentNummer = 20000000;
+private ArrayList<Student> student;
+
+public void Student(String Naam){
+	this.Naam = Naam;
+	this.studentNummer = studentNummer;
+}
+
+private static Integer uniekNummer() { // This raises the unique number of the client each time it is used.
+		int uniekNummer = studentNummer;
+		studentNummer++;
+		return uniekNummer;
+	}
+
+public void inschrijven(String Naam){
+	this.Naam = Naam;
+	uniekNummer();
+}
 
 }
 
