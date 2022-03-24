4146e18c3c7aede8bbbbcd6986c9e02795a2e0c2
Jonat <Jonathon.A.Stamper@gmail.com>
2022-03-24 14:36:55 +0100

    test2

---
+++ src/main/java/Student.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Student{
+    private String Naam;
+    private static Integer studentNummer;
+    private ArrayList<Student> student;
+
+    public void Student(String Naam, Integer studentNummer){
+        this.Naam = Naam;
+        this.studentNummer = studentNummer;
+    }
+
+    public void setStudent(String Naam, Integer studentNummer){
+        this.Naam = Naam;
+        this.studentNummer = studentNummer;
+
+    }
+
+}
