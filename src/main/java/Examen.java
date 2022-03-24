9a8366e17516158e87800bd1714ba6eb7cb2a43d
AyoubAouragh <ayoub.aouragh070@hotmail.com>
2022-03-24 14:40:34 +0100

    meep

---
+++ src/main/java/Examen.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class Examen {
+
+    public final String naam;
+    private String TypeOfExam;
+    private Integer aantalVragen;
+    private ArrayList<Examen> LijstExamen;
+
+
+
+
+    public Examen(String naam, String TypeOfExamen) {
+
+        this.naam = naam;
+    }
+
+}
