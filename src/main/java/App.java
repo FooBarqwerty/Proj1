87a0587cabb4188d6ff84edf01d993ae56dac33e
Jonat <Jonathon.A.Stamper@gmail.com>
2022-03-24 14:34:39 +0100

    test2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,34 +7,7 @@ public class App {
 	}
 }
 
-class Student{
-private String Naam;
-private static Integer studentNummer;
-private ArrayList<Student> student;
-
-public void Student(String Naam, Integer studentNummer){
-	this.Naam = Naam;
-	this.studentNummer = studentNummer;
-}
-
-public void setStudent(String Naam, Integer studentNummer){
-	this.Naam = Naam;
-	this.studentNummer = studentNummer;
-
-}
-
-}
-
-class Examen{
-private String Naam;
-private String TypeOfExam;
-private Integer aantalVragen;
 
 
-}
-
-class results{
-
-}
 
 
