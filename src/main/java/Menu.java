bbefd9e8c60ca6be2aebcc7d834889d8b517b9d9
Jonat <Jonathon.A.Stamper@gmail.com>
2022-03-24 21:25:26 +0100

    test2

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -1,6 +1,16 @@
 class Menu {
 
     private void PrintMenu(){
+        System.out.println("1) Lijst met examens");
+        System.out.println("2) Lijst met studenten");
+        System.out.println("3) Nieuwe student inschrijven");
+        System.out.println("4) Student verwijderen");
+        System.out.println("5) Examen afnemen");
+        System.out.println("6) Is student geslaagd voor test?");
+        System.out.println("7) Welke examens heeft student gehaald?");
+        System.out.println("8) Welke student heeft de meeste examens gehaald?");
+        System.out.println("0) Exit");
+        System.out.println("Uw Keuze: ");
 
     }
 
--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,11 +1,13 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Student{
     private String Naam;
     private static Integer studentNummer;
-    private ArrayList<Student> student;
+    private ArrayList<Student> Studenten = new ArrayList<>();
+    private Results results;
 
-    public void Student(String Naam, Integer studentNummer){
+    public Student(String Naam, Integer studentNummer){
         this.Naam = Naam;
         this.studentNummer = studentNummer;
     }
@@ -13,7 +15,28 @@ public class Student{
     public void setStudent(String Naam, Integer studentNummer){
         this.Naam = Naam;
         this.studentNummer = studentNummer;
+    }
+
+    public void fillList(){
+        Scanner studenten = new Scanner(System.in);
+        System.out.println("Whats your name?");
+        String naam = studenten.nextLine();
+        System.out.println("Whats your studentnumber?");
+        Integer studentNummer = studenten.nextInt();
+        while(studenten.nextInt() > 99999999) {
+
+        }
+
+        Student student = new Student(naam,studentNummer);
+        Studenten.add(student);
+
+
 
     }
 
+    public void removeStudent(String studentNummer){
+
+    }
 }
+
+
