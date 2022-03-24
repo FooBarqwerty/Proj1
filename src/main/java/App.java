604e803ef64f137b544f40d7abd5400fe8c5be6b
HHS-ChristiaandeHaan <18127371@student.hhs.nl>
2022-03-24 14:45:08 +0100

    Added Question.java and added some questions to questionArrayList

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,11 +4,13 @@ public class App {
 
 	public static void main(String[] args){
 		System.out.println(33113);
+		ArrayList<Question> questionArrayList = new ArrayList<Question>();
+		questionArrayList.add(new Question("Wat is the hoofdstad van Nederland?", "Amsterdam"));
+
+		Question sad = new Question("Wat is de hoofdstad van de USA?","Washington DC");
+		questionArrayList.add(sad);
+
+		System.out.println(questionArrayList.get(0).getQuestion());
 
 	}
 }
-
-
-
-
-
