aa241a8f8bdaac25afa8d165a642e1f594e23fdb
AyoubAouragh <ayoub.aouragh070@hotmail.com>
2022-03-24 14:25:01 +0100

    meep

---
+++ src/main/java/Menu.java
@@ -0,0 +1,19 @@
+class Menu {
+
+    private void PrintMenu(){
+
+    }
+
+    private void ListExams(){
+
+    }
+
+    private void ListStudents(){
+
+    }
+
+    private void BestStudent(){
+
+    }
+
+}
