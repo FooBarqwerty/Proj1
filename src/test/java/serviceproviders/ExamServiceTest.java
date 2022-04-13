3a2d4a7641f82bf213f3a5726dd1062285155b19
troykort <troykort@gmail.com>
2022-04-13 12:32:28 +0200

    tests

---
+++ src/test/java/serviceproviders/ExamServiceTest.java
@@ -0,0 +1,32 @@
+package serviceproviders;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExamServiceTest {
+
+    @Test
+    void listExams() {
+    }
+
+    @Test
+    void toets() {
+    }
+
+    @Test
+    void toetsOpen() {
+    }
+
+    @Test
+    void multiVragen() {
+    }
+
+    @Test
+    void jaNeeVragen() {
+    }
+
+    @Test
+    void openVragen() {
+    }
+}
---
+++ src/test/java/serviceproviders/StudentServiceTest.java
@@ -0,0 +1,20 @@
+package serviceproviders;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentServiceTest {
+
+    @Test
+    void nieuweStudent() {
+    }
+
+    @Test
+    void checkStudentNumber() {
+    }
+
+    @Test
+    void removeStudent() {
+    }
+}
