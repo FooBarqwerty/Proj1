ddd2632d942cc77a2860130309e4909f3a3ab345
HHS-ChristiaandeHaan <18127371@student.hhs.nl>
2022-04-14 20:40:59 +0200

    Created testResultsService

--- src/test/java/serviceproviders/ResultsServiceTest.java
+++ src/test/java/serviceproviders/ResultsServiceTest.java
@@ -1,5 +1,7 @@
 package serviceproviders;
 
+import model.Results;
+import model.Student;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,6 +10,17 @@ class ResultsServiceTest {
 
     @Test
     void addResult() {
+        //arrange
+        ResultsService testResultService = new ResultsService();
 
+        DataService addStudentTest = new DataService();
+        Student student = new Student("Jeffery", 372949);
+        var expectedAnswer = true;
+        addStudentTest.addStudent(student);
+
+        //act
+        testResultService.addResult("Test question",3,1);
+        //assert
+        assertFalse(DataService.getCurrentStudent().getLijstResults().isEmpty());
     }
 }
